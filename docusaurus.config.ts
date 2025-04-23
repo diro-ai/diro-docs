beb083346980009ce623bc542e42ba89ea2cde80
diro.ai <duy.nguyen@diro.ai>
2025-04-23 11:27:47 +0700

    Update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'diro-ai', // Usually your GitHub org/user name.
   projectName: 'diro-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
